990845fea8bada5f5fb567deedd845c6b7bd6454
Yam Pallace <yampalas100@gmail.com>
2023-03-22 22:27:48 +0200

    scrapping is working now

--- backend/scraping/scraper.js
+++ backend/scraping/scraper.js
@@ -4,32 +4,31 @@ const pretty = require("pretty");
 const Product = require("../models/product");
 const Stock = require("../models/stock");
 
+const firstResource = 'https://tiktakpet.co.il/en/product-category/%d7%9b%d7%9c%d7%91%d7%99%d7%9d-%d7%a8%d7%a6%d7%95%d7%a2%d7%95%d7%aa-%d7%a7%d7%95%d7%9c%d7%a8%d7%99%d7%9d-%d7%9e%d7%97%d7%a1%d7%95%d7%9e%d7%99%d7%9d-%d7%9e%d7%96%d7%95%d7%9f-%d7%99%d7%91%d7%a9-%d7%91/%d7%9b%d7%9c%d7%99-%d7%90%d7%95%d7%9b%d7%9c-%d7%95%d7%a9%d7%aa%d7%99%d7%94-%d7%9c%d7%9b%d7%9c%d7%91/';
+const secondResource = 'https://tiktakpet.co.il/en/product-category/%d7%9b%d7%9c%d7%91%d7%99%d7%9d-%d7%a8%d7%a6%d7%95%d7%a2%d7%95%d7%aa-%d7%a7%d7%95%d7%9c%d7%a8%d7%99%d7%9d-%d7%9e%d7%97%d7%a1%d7%95%d7%9e%d7%99%d7%9d-%d7%9e%d7%96%d7%95%d7%9f-%d7%99%d7%91%d7%a9-%d7%91/%d7%9e%d7%96%d7%95%d7%9f-%d7%99%d7%91%d7%a9-%d7%9c%d7%9b%d7%9c%d7%91%d7%99%d7%9d/';
+const thirdResource = 'https://tiktakpet.co.il/en/product-category/%d7%9b%d7%9c%d7%91%d7%99%d7%9d-%d7%a8%d7%a6%d7%95%d7%a2%d7%95%d7%aa-%d7%a7%d7%95%d7%9c%d7%a8%d7%99%d7%9d-%d7%9e%d7%97%d7%a1%d7%95%d7%9e%d7%99%d7%9d-%d7%9e%d7%96%d7%95%d7%9f-%d7%99%d7%91%d7%a9-%d7%91/%d7%9e%d7%96%d7%95%d7%9f-%d7%99%d7%91%d7%a9-%d7%9c%d7%92%d7%95%d7%a8%d7%99-%d7%9b%d7%9c%d7%91%d7%99%d7%9d-%d7%9e%d7%95%d7%a0%d7%92-%d7%a8%d7%95%d7%99%d7%90%d7%9c-%d7%a7%d7%a0%d7%99%d7%9f-%d7%9e%d7%96/';
+
+
 const scrape = () => {
     const productsData = []
-    axios.get('https://www.terminalx.com/sports/activity/basketball').then((response) => {
+    axios.get(firstResource).then((response) => {
         extracted(response, productsData, 1);
-    }).then(() => axios.get('https://www.terminalx.com/sports/activity/soccer').then((response) => {
+    }).then(() => axios.get(secondResource).then((response) => {
         extracted(response, productsData, 2);
-    }).then(() => axios.get('https://www.terminalx.com/sports/activity/swimming').then((response) => {
+    }).then(() => axios.get(thirdResource).then((response) => {
         extracted(response, productsData, 3);
-    }).then(() => axios.get('https://www.terminalx.com/sports/activity/running').then((response) => {
-        extracted(response, productsData, 4);
-    }).then(() => axios.get('https://www.terminalx.com/sports/activity/style').then((response) => {
-        extracted(response, productsData, 5);
-    }).then(() => axios.get('https://www.terminalx.com/sports/activity/yoga').then((response) => {
-        extracted(response, productsData, 6);
-    })))))).finally(() => {
+    }))).finally(() => {
         const productsDataNoDuplicates = productsData.filter((arr, index, self) => index === self.findIndex((t) => (t.description === arr.description && t.image === arr.image)))
         const stock = [];
 
         Product.create(productsDataNoDuplicates, (err, res) => {
-            if (err) //console.log(err);
-            //console.log(`Inserted ${res.length} documents`);
+            if (err) console.log(err);
+            console.log(`Inserted ${res.length} documents`);
 
             res.map(product => stock.push({product: product._id, quantity: getRandomStock(10, 500)}));
             Stock.create(stock, (err, res) => {
                 if (err) console.log(err);
-                //console.log(`Inserted ${res.length} documents`);
+                console.log(`Inserted ${res.length} documents`);
             });
         });
     });
@@ -44,13 +43,13 @@ function getRandomStock(min, max) {
 function extracted(response, productsData, category) {
     const body = response.data;
     const $ = cheerio.load(body);
-    const products = $(".listing-product_3mjp");
+    const products = $(".listing-ProductListingGrid_overlayWrapper__A5GZ8");
     products.each((index, el) => {
         const product = {};
-        product.image = $(el).find('div > a > div > div > img').attr('src')
-        product.name = $(el).find('div > div > div > span').text()
-        product.description = $(el).find('a.title_3ZxJ').text()
-        const price = $(el).find('div.final-price_8CiX').text()
+        product.image = $(el).find('div > div > div > a > img').attr('src')
+        product.name = $(el).find('div > div > div > div > h2').text()
+        product.description = $(el).find('div > div > div > div > h2').text()
+        const price = $(el).find('div > div > div > div > div').text()
         product.price = parseInt(price.includes('.') ? price.split('.')[0] : price.split(' ')[0])
         product.category = category;
         product.name.length > 0 && productsData.push(product);
